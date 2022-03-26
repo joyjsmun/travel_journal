6be37489ea8a68937992c4aa146f334b463e2645
joyjsmun <joy.js.mun@gmail.com>
2022-03-26 15:27:03 +0900

    added styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>test</div>
+    <div>
+      <Navbar />
+      <Main />
+      <hr/>
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar () {
+    return (
+        <nav>
+           <img className="navbar--img" src="../images/worldwide.png"/> 
+           <span>my travel journal</span>
+        </nav>
+    )
+}
