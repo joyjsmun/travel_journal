b2fcd10d4cedd4ba6065124ebeab716948c506ad
joyjsmun <joy.js.mun@gmail.com>
2022-03-26 15:35:55 +0900

    added data

--- src/data.js
+++ src/data.js
@@ -1,12 +1,30 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
+        title: "San Francisco",
+        location: "USA",
+        googleMapsUrl: "https://goo.gl/maps/5EoDxCFJsrUZYTWG7",
+        startDate: "01 June, 2022",
+        endDate: "10 June, 2022",
+        description: "San Francisco, officially the City and County of San Francisco, is a cultural, commercial, and financial center in the U.S. state of California",
+        imageUrl: "https://unsplash.com/photos/CcdD-C2ii-8"
+    },
+    {
+        title: "New York",
+        location: "USA",
+        googleMapsUrl: "https://goo.gl/maps/zvVwzAW5GULsyo3Y7",
+        startDate: "17 June, 2022",
+        endDate: "01 July, 2022",
+        description: "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square.",
+        imageUrl: "https://unsplash.com/photos/Y84-eo8drzk"
+    },
+    {
+        title: "Portland",
+        location: "USA",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    }
+        startDate: "17 Oct, 2022",
+        endDate: "29 Oct, 2022",
+        description: "Portland, Oregon’s largest city, sits on the Columbia and Willamette rivers, in the shadow of snow-capped Mount Hood. It’s known for its parks, bridges and bicycle paths, as well as for its eco-friendliness and its microbreweries and coffeehouses. Iconic Washington Park encompasses sites from the formal Japanese Garden to Oregon Zoo and its railway. The city hosts thriving art, theater and music scenes.",
+        imageUrl: "https://unsplash.com/photos/4ys0ENh4PD8"
+    },
 ]
